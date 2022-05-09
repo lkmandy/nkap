3903141d2b975e35056f0454ab67c3c9f3f0cb98
Amanda Shafack <shafack.likhene@gmail.com>
2022-05-09 18:02:53 +0100

    Fix old android embedding problem

--- android/app/src/main/kotlin/com/lkmandy/nkap/MainActivity.kt
+++ android/app/src/main/kotlin/com/lkmandy/nkap/MainActivity.kt
@@ -3,4 +3,8 @@ package com.lkmandy.nkap
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 }
