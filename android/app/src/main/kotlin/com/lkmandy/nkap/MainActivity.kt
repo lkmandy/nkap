98bcf6f4aed208be1d133ac65afdb242b5bb80a8
Amanda Shafack <shafack.likhene@gmail.com>
2022-05-11 08:13:04 +0100

    Fix kotlin based configuration bug

--- android/app/src/main/kotlin/com/lkmandy/nkap/MainActivity.kt
+++ android/app/src/main/kotlin/com/lkmandy/nkap/MainActivity.kt
@@ -3,8 +3,4 @@ package com.lkmandy.nkap
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun attachBaseContext(base: Context) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
 }
